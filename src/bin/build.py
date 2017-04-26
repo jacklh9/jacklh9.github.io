801e25c9a58f1fb31000b8b166b37082cb102a83
Jack L. Hamilton <jacklh@gmail.com>
2017-04-26 13:26:57 -0700

    Add working listing of html files

--- src/bin/build.py
+++ src/bin/build.py
@@ -1,22 +1,38 @@
-import sys, getopt, os
+import getopt, glob, os, sys
 from subprocess import check_output
 from os.path import basename
+
+
+def build_dir(inputdir, outputdir, links):
+    print("Building dir:", inputdir)
+    srcs = glob.glob(os.path.join(inputdir, '*.src'))
+    for src in srcs:
+      base_name = os.path.splitext(src)[0]
+      print("HTML NAME:", base_name + ".html")
+      html = open(os.path.join(outputdir, base_name + ".html"))
+      # combine parent links as well
+      
+      #for link in links.extend(glob.glob(os.path.join(dir,"*.link")))
+      pass
+      # TODO
+
+
+def build_dirs(inputdir, outputdir, links):
+    build_dir(inputdir=inputdir, outputdir=outputdir, links=links)
+    subdirs = get_subdirs(dir=inputdir)
+
+    # process subdirs
+    if len(subdirs) > 0:
+        for dir in subdirs:
+            path = os.path.join(inputdir, basename(dir))
+            build_dirs(inputdir=path, outputdir=outputdir, links=links)
 
 def get_subdirs(dir):
     dirs = [d for d in os.listdir(path=dir) if os.path.isdir(os.path.join(dir,d))]
     return dirs
 
-def build_dirs(inputdir, outputdir):
-    subdirs = get_subdirs(dir=inputdir)
-
-    if len(subdirs) > 0:
-        for dir in subdirs:
-            path = os.path.join(inputdir, basename(dir))
-            build_dirs(inputdir=path, outputdir=outputdir)
-
-
 def print_usage():
-    print(os.path.basename(sys.argv[0]), "-i <input-dir> -o <output-dir>")
+    print(basename(sys.argv[0]), "-i <input-dir> -o <output-dir>")
     print("\n\tRun command from directory containing build files and")
     print("\tbuild subdirectories. Default files should be in")
     print("\tdirectory entitled 'default'.")
@@ -46,17 +62,8 @@ def main(argv):
         print_usage()
         sys.exit(2)
 
-    build_dirs(inputdir=inputdir, outputdir=outputdir)
+    build_dirs(inputdir=inputdir, outputdir=outputdir, links=[])
 
 if __name__ == "__main__":
    main(sys.argv[1:])
 
-
-
-def main():
-
-    try:    
-        print("Dirs:",get_dirs(output_dir=sys.argv[1]))
-    except ArgumentError as e:
-        print(e.args)
-        print_usage()
